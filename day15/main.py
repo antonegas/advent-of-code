104fd301435ff01793f8190e9a142b6cf57549df
antonegas <32789589+antonegas@users.noreply.github.com>
2023-12-21 13:55:44 +0100

    Day 15 part 1

--- day15/main.py
+++ day15/main.py
@@ -1,5 +1,16 @@
+def hashed(string):
+    res = 0
+    for char in string:
+        res += ord(char)
+        res *= 17
+        res %= 256
+    return res
+
 if __name__ == "__main__":
     import os
     __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
     data = open(os.path.join(__location__, "input.txt"), "r").read()
+    sequence = list(data.split(","))
+
+    print("Part 1:", sum([hashed(string) for string in sequence]))
