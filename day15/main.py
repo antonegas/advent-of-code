fc288b1ef42405981f0d6091ab0e2aa7baf0360a
antonegas <32789589+antonegas@users.noreply.github.com>
2023-12-22 23:03:36 +0100

    Day 15 part 2

--- day15/main.py
+++ day15/main.py
@@ -6,6 +6,22 @@ def hashed(string):
         res %= 256
     return res
 
+def initialization(sequence):
+    boxes = [dict() for _ in range(256)]
+    for string in sequence:
+        label, remove_operation, focal_length = hashmap(string)
+        hashed_label = hashed(label)
+        if not remove_operation:
+            boxes[hashed_label][label] = focal_length
+        elif label in boxes[hashed_label]:
+            del boxes[hashed_label][label]
+    return boxes
+
+def hashmap(string):
+    remove_operation = "-" in string
+    label, focal_length = list(string.split("-")) if remove_operation else list(string.split("="))
+    return label, remove_operation, focal_length
+
 if __name__ == "__main__":
     import os
     __location__ = os.path.realpath(
@@ -14,3 +30,4 @@ if __name__ == "__main__":
     sequence = list(data.split(","))
 
     print("Part 1:", sum([hashed(string) for string in sequence]))
+    print("Part 2:", sum([sum([(i + 1) * (j + 1) * int(focal_length) for j, focal_length in enumerate(box.values())]) for i, box in enumerate(initialization(sequence))]))
