a1db88cc8ff1709b159f565a58516ca5abc50802
antonegas <32789589+antonegas@users.noreply.github.com>
2023-12-24 00:14:13 +0100

    Part 1/2 print

--- day17/main.py
+++ day17/main.py
@@ -3,3 +3,7 @@ if __name__ == "__main__":
     __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
     data = open(os.path.join(__location__, "input.txt"), "r").read()
+    heat_loss_map = list(list(x) for x in data.splitlines())
+
+    print("Part 1:", 0)
+    print("Part 2:", 0)
--- day24/main.py
+++ day24/main.py
@@ -3,3 +3,6 @@ if __name__ == "__main__":
     __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
     data = open(os.path.join(__location__, "input.txt"), "r").read()
+
+    print("Part 1:", 0)
+    print("Part 2:", 0)
