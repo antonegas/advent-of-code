548079c1a4c1f2db70384da179765f346a932c82
antonegas <32789589+antonegas@users.noreply.github.com>
2023-12-30 22:03:58 +0100

    Part 2

--- day21/main.py
+++ day21/main.py
@@ -5,12 +5,13 @@ def get_starting_point(garden_map):
                 return (x, y)
 
 def get_all_reachable(garden_map, steps):
+    res = list()
     reachable_even = {get_starting_point(garden_map)}
     reachable_odd = set()
     new_reachable = reachable_even
 
-    for i in range(steps):
-        even = (i % 2) == 1
+    for i in range(1, max(steps) + 1):
+        even = (i % 2) == 0
         reachables = set()
         for reachable in new_reachable:
             reachables.update(get_currently_reachable(garden_map, reachable))
@@ -19,19 +20,32 @@ def get_all_reachable(garden_map, steps):
             reachable_even = reachable_even.union(reachables)
         else:
             reachable_odd = reachable_odd.union(reachables)
+        if i in steps:
+            res.append(len(reachable_even if even else reachable_odd))
 
-    return reachable_even if (steps % 2) == 0 else reachable_odd
+    return res
 
 def get_currently_reachable(garden_map, coordinate):
     res = list()
     x, y = coordinate
+    height = len(garden_map)
+    width = len(garden_map[0])
 
     for reachable in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
         new_x, new_y = reachable
-        if new_x >= 0 and new_x < len(garden_map[0]) and new_y >= 0 and new_y < len(garden_map) and garden_map[new_y][new_x] != "#":
+        if garden_map[new_y % height][new_x % width] != "#":
             res.append(reachable)
 
-    return res
+    return tuple(res)
+
+def quadratic_coefficients(y: tuple[int, int, int]):
+    y0, y1, y2 = y
+    return y0, (-3 * y0 + 4 * y1 - y2) // 2, (y0 - 2 * y1 + y2) // 2
+
+def calculate_quadratic(x, coefficients):
+    c, b, a = coefficients
+    return c + b * x + a * x**2
+
 
 if __name__ == "__main__":
     import os
@@ -40,5 +54,5 @@ if __name__ == "__main__":
     data = open(os.path.join(__location__, "input.txt"), "r").read()
     garden_map = tuple(data.split("\n"))
 
-    print("Part 1:", len(get_all_reachable(garden_map, 64)))
-    print("Part 2:", 0)
+    print("Part 1:", get_all_reachable(garden_map, [64])[0])
+    print("Part 2:", calculate_quadratic(202300, quadratic_coefficients(get_all_reachable(garden_map, [65, 196, 327]))))
