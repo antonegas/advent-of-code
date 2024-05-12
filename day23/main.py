00c08abf74ac684a68b298b360bc3bc93b2dab6a
antonegas <32789589+antonegas@users.noreply.github.com>
2024-05-12 16:16:04 +0200

    Part 1 and 2 works but part 2 is slow

--- day23/main.py
+++ day23/main.py
@@ -1,111 +1,122 @@
-def get_paths(forest):
-    START = (1, 0)
-    END = (len(forest[0]) - 2, len(forest) - 1)
-    checked_slopes = set()
-    current_paths = [START]
-    paths = list()
+from collections import defaultdict
 
-    while len(current_paths) != 0:
-        current_start = current_paths.pop(0)
-        current_path = expand_path(current_start, forest, END)
-        paths.append(current_path)
-        current_end = current_path[-1]
-        slopes = get_slopes(current_end[0], current_end[1], forest)
-        current_paths.extend([slope for slope in slopes if slope not in checked_slopes])
-        checked_slopes.update(set(slopes))
+def is_intersection(coordinate, forest):
+    return len(get_connected(coordinate, forest)) > 2
+
+def is_wall(coordinate, forest):
+    x, y = coordinate
+    return forest[y][x] == "#"
+
+def get_end(forest):
+    return (len(forest[0]) - 2, len(forest) - 1)
+
+def get_start():
+    return (1, 0)
+
+def is_end(coordinate, forest):
+    return get_end(forest) == coordinate
+
+def is_start(coordinate):
+    return get_start() == coordinate
+
+def get_adjecent(coordinate):
+    x, y = coordinate
+    return {(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)}
+
+def get_right_down(coordinate):
+    x, y = coordinate
+    return {(x + 1, y), (x, y + 1)}
+
+def in_bounds(coordinate, forest):
+    x, y = coordinate
+    return x >= 0 and y >= 0 and x < len(forest[0]) and y < len(forest)
+
+def get_connected(coordinate, forest):
+    return set(adjecent for adjecent in get_adjecent(coordinate) if in_bounds(adjecent, forest) and not is_wall(adjecent, forest))
+
+def get_slippery_connected(coordinate, forest):
+    return get_connected(coordinate, forest).intersection(get_right_down(coordinate))
+
+def get_next(coordinate, forest, visited):
+    return get_connected(coordinate, forest).difference(visited)
+
+def get_path(intersection, entry_point, forest):
+    visited = {intersection}
+    path = []
+    current = entry_point
+
+    while not is_end(current, forest) and not is_start(current) and not is_intersection(current, forest):
+        visited.add(current)
+        path.append(current)
+        current = get_next(current, forest, visited).pop()
+
+    path.append(current)
+    return path
+
+def place(coordinate, forest, letter):
+    x, y = coordinate
+    forest[y] = forest[y][:x] + letter + forest[y][x + 1:]
+    return forest
+
+def print_forest(forest):
+    for row in forest:
+        print(row)
+
+def get_paths1(forest): # bfs
+    visited = set()
+    queue = list()
+    queue.append(get_start())
+    visited.add(get_start())
+    paths = defaultdict(lambda: dict())
+
+    while len(queue) != 0:
+        current_intersection = queue.pop(0)
+        for exit_point in get_slippery_connected(current_intersection, forest):
+            path = get_path(current_intersection, exit_point, forest)
+            other_intersection = path[-1]
+
+            paths[current_intersection][other_intersection] = len(path)
+
+            if other_intersection not in visited:
+                visited.add(other_intersection)
+                queue.append(other_intersection)
 
     return paths
 
-def dfs1(path, paths):
-    return len(path) + max(*[dfs1(connected_path, paths) for connected_path in connected_paths1(path, paths)], 0, 0)
+def get_paths2(forest): # bfs
+    visited = set()
+    queue = list()
+    queue.append(get_start())
+    visited.add(get_start())
+    paths = defaultdict(lambda: dict())
 
+    while len(queue) != 0:
+        current_intersection = queue.pop(0)
+        for exit_point in get_connected(current_intersection, forest):
+            path = get_path(current_intersection, exit_point, forest)
+            other_intersection = path[-1]
 
-def connected_paths1(path_from, paths):
-    x, y = path_from[-1]
-    possible_connections = [(x + 1, y), (x, y + 1)]
-    res = list()
+            paths[current_intersection][other_intersection] = len(path)
+            paths[other_intersection][current_intersection] = len(path)
 
-    for path_to in paths:
-        path_to_start = path_to[0]
-        if path_to_start in possible_connections:
-            res.append(path_to)
+            if other_intersection not in visited:
+                visited.add(other_intersection)
+                queue.append(other_intersection)
 
-    return res
+    return paths
 
-def dfs2(path, paths, visited):
-    temp = connected_paths2(path, paths, visited)
-    return len(path) + max(*[dfs2(connected_path, paths, visited.union({tuple(path)})) for connected_path in temp], 0, 0)
+def dfs(intersection, paths, visited, end):
+    if intersection == end:
+        return 0
 
+    visited.add(intersection)
 
-def connected_paths2(path_from, paths, visited):
-    x, y = path_from[-1]
-    possible_connections = [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
-    res = list()
+    possible_paths = set(paths[intersection]).difference(visited)
 
-    for path_to in paths:
-        path_to_start = path_to[0]
-        if path_to_start in possible_connections and tuple(path_to) not in visited:
-            res.append(path_to)
+    if len(possible_paths) == 0:
+        return float('-inf')
 
-    return res
-
-def expand_path(start, forest, end):
-    path = list()
-    checked = set()
-    current = start
-
-    while (current == start or not is_slope(current[0], current[1], forest)) and current != (0, 0) and current != end:
-        path.append(current)
-        checked.add(current)
-        current = get_next(current[0], current[1], forest, checked)
-    
-    return path
-
-def is_slope(x, y, forest):
-    SLOPES = "<>v^"
-    return forest[y][x] in SLOPES
-
-def get_next_sliding(x, y, forest):
-    slope = forest[y][x]
-
-    if slope == "<":
-        return (x - 1, y)
-    elif slope == ">":
-        return (x + 1, y)
-    elif slope == "^":
-        return (x, y - 1)
-    else:
-        return (x, y + 1)
-
-def get_slopes(x, y, forest):
-    res = []
-
-    if forest[y][x - 1] == "<":
-        res.append((x - 1, y))
-    if forest[y][x + 1] == ">":
-        res.append((x + 1, y))
-    if forest[y - 1][x] == "^":
-        res.append((x, y - 1))
-    if forest[y + 1][x] == "v":
-        res.append((x, y + 1))
-
-    return res
-
-def get_next(x, y, forest, checked):
-    if (x, y) == (1, 0):
-        return (1, 1)
-    elif is_slope(x, y, forest):
-        return get_next_sliding(x, y, forest)
-    elif (x - 1, y) not in checked and forest[y][x - 1] != "#" and forest[y][x - 1] != ">":
-        return (x - 1, y)
-    elif (x + 1, y) not in checked and forest[y][x + 1] != "#" and forest[y][x + 1] != "<":
-        return (x + 1, y)
-    elif (x, y - 1) not in checked and forest[y - 1][x] != "#" and forest[y - 1][x] != "v":
-        return (x, y - 1)
-    elif (x, y + 1) not in checked and forest[y + 1][x] != "#" and forest[y + 1][x] != "^":
-        return (x, y + 1)
-    
-    return (0, 0)
+    return max([dfs(possible_path, paths, visited.copy(), end) + paths[intersection][possible_path] for possible_path in possible_paths])
 
 if __name__ == "__main__":
     import os
@@ -113,7 +124,6 @@ if __name__ == "__main__":
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
     data = open(os.path.join(__location__, "input.txt"), "r").read()
     forest = [x for x in data.split("\n")]
-    paths = get_paths(forest)
 
-    print("Part 1:", dfs1(paths[0], paths))
-    print("Part 2:", dfs2(paths[0], paths, set()))
+    print("Part 1:", dfs(get_start(), get_paths1(forest), set(), get_end(forest)))
+    print("Part 2:", dfs(get_start(), get_paths2(forest), set(), get_end(forest)))
