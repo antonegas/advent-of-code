96cd090ee2ffe1e1a48a2e9a4df0e3576e2ae3ee
antonegas <32789589+antonegas@users.noreply.github.com>
2024-11-20 12:23:02 +0100

    Preparing for 2024

---
+++ 2024/day17/main.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    import os
+    __location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    data = open(os.path.join(__location__, "input.txt"), "r").read()
