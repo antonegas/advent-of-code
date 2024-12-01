33a03ed6b7085cdf949479eca082d7b099ad1bd1
antonegas <32789589+antonegas@users.noreply.github.com>
2024-12-01 12:48:36 +0100

    Went back in time by 2 years

---
+++ 2022/day01/main.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    import os
+    __location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    data = open(os.path.join(__location__, "input.txt"), "r").read()
+
+    print("Part 1:")
+    print("Part 2:")
