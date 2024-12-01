16316340ce283d059a0ddf6c4d8395997c6b5880
antonegas <32789589+antonegas@users.noreply.github.com>
2024-12-01 14:28:22 +0100

    Raced through the first three days of 2022

--- 2022/day02/main.py
+++ 2022/day02/main.py
@@ -3,3 +3,71 @@ if __name__ == "__main__":
     __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
     data = open(os.path.join(__location__, "input.txt"), "r").read()
+    inp = [[y for y in x.split(" ")] for x in data.split("\n")]
+
+    # print(inp)
+
+    sc = {
+        "X": 1,
+        "Y": 2,
+        "Z": 3,
+    }
+
+    w = {
+        "A": {
+            "X": 3,
+            "Y": 6,
+            "Z": 0,
+        },
+        "B": {
+            "X": 0,
+            "Y": 3,
+            "Z": 6,
+        },
+        "C": {
+            "X": 6,
+            "Y": 0,
+            "Z": 3,
+        }
+    }
+
+    sc2 = {
+        "X": 0,
+        "Y": 3,
+        "Z": 6,
+    }
+
+    sc3 = {
+        "A": {
+            0: "Z",
+            3: "X",
+            6: "Y",
+        },
+        "B": {
+            0: "X",
+            3: "Y",
+            6: "Z",
+        },
+        "C": {
+            0: "Y",
+            3: "Z",
+            6: "X",
+        }
+    }
+
+    p1 = 0
+
+    p2 = 0
+
+    for o, y in inp:
+        # print(sc[y], w[o][y])
+        p1 += sc[y]
+        p1 += w[o][y]
+
+    for o, y in inp:
+        t = sc2[y]
+        p2 += t
+        p2 += sc[sc3[o][t]]
+
+    print("Part 1:", p1)
+    print("Part 2:", p2)
--- 2022/day03/main.py
+++ 2022/day03/main.py
@@ -1,5 +1,78 @@
+from collections import defaultdict
+
 if __name__ == "__main__":
     import os
     __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
     data = open(os.path.join(__location__, "input.txt"), "r").read()
+    rs = [x for x in data.split("\n")]
+
+    p1 = 0
+    p2 = 0
+
+    # print(rs)
+
+    for r in rs:
+        s1 = r[:int(len(r) / 2)]
+        s2 = r[int(len(r) / 2):]
+        # print(s1, s2)
+        st = set()
+        for l in s1:
+            st.add(l)
+        for l in s2:
+            if l in st:
+                if l.isupper():
+                    t = ord(l) - ord('A') + 27
+                    # print(t, l)
+                    p1 += t
+                else:
+                    t = ord(l) - ord('a') + 1
+                    # print(t, l)
+                    p1 += t
+                break
+
+    c = int(len(rs) / 3)
+
+    for i in range(0, len(rs), 3):
+        d = defaultdict(lambda: 0)
+        r1 = rs[i]
+        r2 = rs[i + 1]
+        r3 = rs[i + 2]
+
+        # print(r1)
+        # print(r2)
+        # print(r3)
+        # break
+
+        m = min([len(k) for k in [r1, r2, r3]])
+
+        print(m)
+
+        for l in r1:
+            if d[l] == 0:
+                d[l] += 1
+
+        for l in r2:
+            if d[l] == 1:
+                d[l] += 1
+
+        for l in r3:
+            if d[l] == 2:
+                print(l)
+                if l.isupper():
+                    t = ord(l) - ord('A') + 27
+                    # print(t, l)
+                    p2 += t
+                else:
+                    t = ord(l) - ord('a') + 1
+                    # print(t, l)
+                    p2 += t
+                break
+            
+
+        # for j in range(m):
+        #     if r1 == r2 and r2 == r3:
+                
+
+    print("Part 1:", p1)
+    print("Part 2:", p2)
