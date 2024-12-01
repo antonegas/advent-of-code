b07e6446983b96b966460501e4878476bdd9f8c4
antonegas <32789589+antonegas@users.noreply.github.com>
2024-12-01 12:47:33 +0100

    Removed copied code

--- 2018/day01/main.py
+++ 2018/day01/main.py
@@ -3,7 +3,6 @@ if __name__ == "__main__":
     __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
     data = open(os.path.join(__location__, "input.txt"), "r").read()
-    lists = [[int(y) for y in x.split("   ")] for x in data.split("\n")]
 
-    print("Part 1:", sum(get_differences(*sort_both(*seperate_lists(lists)))))
-    print("Part 2:", sum(get_similarity_score(*count_right_occurences(*seperate_lists(lists)))))
+    print("Part 1:")
+    print("Part 2:")
