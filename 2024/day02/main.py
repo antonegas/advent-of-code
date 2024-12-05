107229f05290a0d6eb92caae98912ab5e780b221
antonegas <32789589+antonegas@users.noreply.github.com>
2024-12-05 09:33:27 +0100

    Cleaned up day 2

--- 2024/day02/main.py
+++ 2024/day02/main.py
@@ -22,49 +22,5 @@ if __name__ == "__main__":
     data = open(os.path.join(__location__, "input.txt"), "r").read()
     reports = [[int(y) for y in x.split(" ")] for x in data.split("\n")]
 
-
-
-    # p1 = 0
-    # p2 = 0
-
-    # for i in lev:
-    #     t = True
-    #     f = True
-    #     h = False
-        
-    #     for b in range(len(i)):
-    #         a = i[:b] + i[b+1:]
-    #         t = True
-    #         h = False
-    #         f = True
-    #         if a == [1, 2, 4, 5]:
-    #             print("hey", b)
-    #         for n in range(len(a)- 1):
-    #             d = (a[n] - a[n+1])
-    #             g = d > 0
-    #             if d == 0:
-    #                 t = False
-    #                 break
-    #             if f:
-    #                 f = False
-    #                 h = g
-    #             elif g != h:
-    #                 t = False
-    #                 break
-
-    #             if (abs(d) > 3):
-    #                 t = False
-    #                 break
-    #         if t:
-    #             break
-
-    #     if t:
-    #         p1 += 1
-
-
-    # for i in range(1, len(reports[0]) - 1):
-    #     print(safe(reports[0][i], reports[0][i+1], is_increasing(reports[0][0], reports[0][1])))
-
-
     print("Part 1:", len([report for report in reports if all([safe(report[i], report[i + 1], is_increasing(report[0], report[1])) for i in range(0, len(report) - 1)])]))
     print("Part 2:", len([report for report in reports if any([safe_dampend(report, index) for index in range(len(report))])]))
