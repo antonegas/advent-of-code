a8ff28a3743466f75e0bed92ad071f5a38e3ace5
antonegas <32789589+antonegas@users.noreply.github.com>
2024-12-05 09:45:36 +0100

    Cleaned up day 4 part 1

--- 2024/day04/main.py
+++ 2024/day04/main.py
@@ -6,47 +6,79 @@ def xmas(g, x, y):
         return 0
     
     res = 0
+
+    if in_bounds(g, x - 1, y) and in_bounds(g, x - 2, y) and in_bounds(g, x - 3, y):
+        if g[y][x - 1] + g[y][x - 2] + g[y][x - 3] == "MAS":
+            res += 1
     
-    if in_bounds(g, x - 1, y) and g[y][x - 1] == "M":
-        if in_bounds(g, x - 2, y) and g[y][x - 2] == "A":
-            if in_bounds(g, x - 3, y) and g[y][x - 3] == "S":
-                res += 1
+    # if in_bounds(g, x - 1, y) and g[y][x - 1] == "M":
+    #     if in_bounds(g, x - 2, y) and g[y][x - 2] == "A":
+    #         if in_bounds(g, x - 3, y) and g[y][x - 3] == "S":
+    #             res += 1
 
-    if in_bounds(g, x + 1, y) and g[y][x + 1] == "M":
-        if in_bounds(g, x + 2, y) and g[y][x + 2] == "A":
-            if in_bounds(g, x + 3, y) and g[y][x + 3] == "S":
-                res += 1
+    if in_bounds(g, x + 1, y) and in_bounds(g, x + 2, y) and in_bounds(g, x + 3, y):
+        if g[y][x + 1] + g[y][x + 2] + g[y][x + 3] == "MAS":
+            res += 1
+
+    # if in_bounds(g, x + 1, y) and g[y][x + 1] == "M":
+    #     if in_bounds(g, x + 2, y) and g[y][x + 2] == "A":
+    #         if in_bounds(g, x + 3, y) and g[y][x + 3] == "S":
+    #             res += 1
+
+    if in_bounds(g, x, y + 1) and in_bounds(g, x, y + 2) and in_bounds(g, x , y + 3):
+        if g[y + 1][x] + g[y + 2][x] + g[y + 3][x] == "MAS":
+            res += 1
 
 
-    if in_bounds(g, x, y + 1) and g[y + 1][x] == "M":
-        if in_bounds(g, x, y + 2) and g[y + 2][x] == "A":
-            if in_bounds(g, x , y + 3) and g[y + 3][x] == "S":
-                res += 1
+    # if in_bounds(g, x, y + 1) and g[y + 1][x] == "M":
+    #     if in_bounds(g, x, y + 2) and g[y + 2][x] == "A":
+    #         if in_bounds(g, x , y + 3) and g[y + 3][x] == "S":
+    #             res += 1
 
-    if in_bounds(g, x, y - 1) and g[y - 1][x] == "M":
-        if in_bounds(g, x, y - 2) and g[y - 2][x] == "A":
-            if in_bounds(g, x , y - 3) and g[y - 3][x] == "S":
-                res += 1
+    if in_bounds(g, x, y - 1) and in_bounds(g, x, y - 2) and in_bounds(g, x , y - 3):
+        if "X" + g[y - 1][x] + g[y - 2][x] + g[y - 3][x] == "XMAS":
+            res += 1
 
-    if in_bounds(g, x - 1, y - 1) and g[y - 1][x - 1] == "M":
-        if in_bounds(g, x - 2, y - 2) and g[y - 2][x - 2] == "A":
-            if in_bounds(g, x - 3, y - 3) and g[y - 3][x - 3] == "S":
-                res += 1
+    # if in_bounds(g, x, y - 1) and g[y - 1][x] == "M":
+    #     if in_bounds(g, x, y - 2) and g[y - 2][x] == "A":
+    #         if in_bounds(g, x , y - 3) and g[y - 3][x] == "S":
+    #             res += 1
 
-    if in_bounds(g, x + 1, y - 1) and g[y - 1][x + 1] == "M":
-        if in_bounds(g, x + 2, y - 2) and g[y - 2][x + 2] == "A":
-            if in_bounds(g, x + 3, y - 3) and g[y - 3][x + 3] == "S":
-                res += 1
+    if in_bounds(g, x - 1, y - 1) and in_bounds(g, x - 2, y - 2) and in_bounds(g, x - 3, y - 3):
+        if "X" + g[y - 1][x - 1] + g[y - 2][x - 2] + g[y - 3][x - 3] == "XMAS":
+            res += 1
 
-    if in_bounds(g, x + 1, y + 1) and g[y + 1][x + 1] == "M":
-        if in_bounds(g, x + 2, y + 2) and g[y + 2][x + 2] == "A":
-            if in_bounds(g,x + 3, y + 3) and g[y + 3][x + 3] == "S":
-                res += 1
+    # if in_bounds(g, x - 1, y - 1) and g[y - 1][x - 1] == "M":
+    #     if in_bounds(g, x - 2, y - 2) and g[y - 2][x - 2] == "A":
+    #         if in_bounds(g, x - 3, y - 3) and g[y - 3][x - 3] == "S":
+    #             res += 1
 
-    if in_bounds(g, x - 1, y + 1) and g[y + 1][x - 1] == "M":
-        if (in_bounds(g, x - 2, y + 2) and g[y + 2][x - 2]) == "A":
-            if in_bounds(g, x - 3, y + 3) and g[y + 3][x - 3] == "S":
-                res += 1
+    if in_bounds(g, x + 1, y - 1) and in_bounds(g, x + 1, y - 1) and in_bounds(g, x + 3, y - 3):
+        if "X" + g[y - 1][x + 1] + g[y - 2][x + 2] + g[y - 3][x + 3] == "XMAS":
+            res += 1
+
+    # if in_bounds(g, x + 1, y - 1) and g[y - 1][x + 1] == "M":
+    #     if in_bounds(g, x + 1, y - 1) and g[y - 2][x + 2] == "A":
+    #         if in_bounds(g, x + 3, y - 3) and g[y - 3][x + 3] == "S":
+    #             res += 1
+
+    if in_bounds(g, x + 1, y + 1) and in_bounds(g, x + 2, y + 2) and in_bounds(g,x + 3, y + 3):
+        if "X" + g[y + 1][x + 1] + g[y + 2][x + 2] + g[y + 3][x + 3] == "XMAS":
+            res += 1
+
+    # if in_bounds(g, x + 1, y + 1) and g[y + 1][x + 1] == "M":
+    #     if in_bounds(g, x + 2, y + 2) and g[y + 2][x + 2] == "A":
+    #         if in_bounds(g,x + 3, y + 3) and g[y + 3][x + 3] == "S":
+    #             res += 1
+
+    if in_bounds(g, x - 1, y + 1) and in_bounds(g, x - 2, y + 2) and in_bounds(g, x - 3, y + 3):
+        if "X" + g[y + 1][x - 1] + g[y + 2][x - 2] + g[y + 3][x - 3] == "XMAS":
+            res += 1
+
+    # if in_bounds(g, x - 1, y + 1) and g[y + 1][x - 1] == "M":
+    #     if (in_bounds(g, x - 2, y + 2) and g[y + 2][x - 2]) == "A":
+    #         if in_bounds(g, x - 3, y + 3) and g[y + 3][x - 3] == "S":
+    #             res += 1
 
     return res
 
