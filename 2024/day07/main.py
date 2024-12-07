84c33bca38f8a10f1b4c0ea7dd527f915a1ce7c7
antonegas <32789589+antonegas@users.noreply.github.com>
2024-12-07 07:39:46 +0100

    Day 7 done but slow

--- 2024/day07/main.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-    import os
-    __location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    data = open(os.path.join(__location__, "input.txt"), "r").read()
