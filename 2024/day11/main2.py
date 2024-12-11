9d0e96aa170d27e3fc9a33cda7d2e9042c35f4a9
antonegas <32789589+antonegas@users.noreply.github.com>
2024-12-11 06:42:03 +0100

    Fixed giving part 2 answer for both parts

--- 2024/day11/main2.py
+++ 2024/day11/main2.py
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     data = open(os.path.join(__location__, "input.txt"), "r").read()
     stones = [int(x) for x in data.split(" ")]
 
-    print("Part 1:", sum([blink_n(stone, 75) for stone in stones]))
+    print("Part 1:", sum([blink_n(stone, 25) for stone in stones]))
     print("Part 2:", sum([blink_n(stone, 75) for stone in stones]))
