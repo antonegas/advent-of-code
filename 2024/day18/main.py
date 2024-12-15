ad55ef97d823980bd8699196162ad364a317bcad
antonegas <32789589+antonegas@users.noreply.github.com>
2024-12-15 05:56:38 +0100

    Added boilerplate to remaining days of 2024

--- 2024/day18/main.py
+++ 2024/day18/main.py
@@ -3,3 +3,13 @@ if __name__ == "__main__":
     __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
     data = open(os.path.join(__location__, "input.txt"), "r").read()
+
+
+
+    part1 = 0
+    part2 = 0
+
+
+
+    print("Part 1:", part1)
+    print("Part 2:", part2)
