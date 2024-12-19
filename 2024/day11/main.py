241b0de344f4e214224b229827d9f9af0000b127
antonegas <32789589+antonegas@users.noreply.github.com>
2024-12-19 06:25:43 +0100

    Removed day 11 solution which didn't work

--- 2024/day11/main.py
+++ 2024/day11/main.py
@@ -1,3 +1,18 @@
+from functools import cache
+
+@cache
+def blink_n(stone, loops):
+    if loops == 0:
+        return 1
+    
+    if stone == 0:
+        return blink_n(1, loops - 1)
+    elif len(str(stone)) % 2 == 0:
+        t = str(stone)
+        return blink_n(int(t[:len(t)//2]), loops - 1) + blink_n(int(t[len(t)//2:]), loops - 1)
+    else:
+        return blink_n(stone * 2024, loops - 1)
+
 if __name__ == "__main__":
     import os
     __location__ = os.path.realpath(
@@ -5,23 +20,5 @@ if __name__ == "__main__":
     data = open(os.path.join(__location__, "input.txt"), "r").read()
     stones = [int(x) for x in data.split(" ")]
 
-    part1 = 0
-    part2 = 0
-
-    stones1 = stones[:]
-
-    for i in range(75):
-        r = list()
-        for stone in stones1:
-            if stone == 0:
-                r.append(1)
-            elif len(str(stone)) % 2 == 0:
-                t = str(stone)
-                r.append(int(t[:len(t)//2]))
-                r.append(int(t[len(t)//2:]))
-            else:
-                r.append(stone * 2024)
-        stones1 = r
-
-    print("Part 1:", len(stones1))
-    print("Part 2:", part2)
+    print("Part 1:", sum([blink_n(stone, 25) for stone in stones]))
+    print("Part 2:", sum([blink_n(stone, 75) for stone in stones]))
