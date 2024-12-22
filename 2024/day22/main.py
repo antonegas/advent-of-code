d453e33add58bf78382042ecb639e25f374172c9
antonegas <32789589+antonegas@users.noreply.github.com>
2024-12-22 08:32:37 +0100

    Cleaned day 22

--- 2024/day22/main.py
+++ 2024/day22/main.py
@@ -1,11 +1,9 @@
-from functools import cache
-from collections import defaultdict, deque
+from collections import defaultdict
 
-@cache
 def evolve(secret_number):
-    t1 = (secret_number ^ (secret_number << 6)) & 16777215
-    t2 = (t1 ^ (t1 >> 5)) & 16777215
-    return (t2 ^ (t2 << 11)) & 16777215
+    secret_number = (secret_number ^ (secret_number << 6)) & 16777215
+    secret_number = (secret_number ^ (secret_number >> 5)) & 16777215
+    return (secret_number ^ (secret_number << 11)) & 16777215
 
 if __name__ == "__main__":
     import os
@@ -17,32 +15,35 @@ if __name__ == "__main__":
     part1 = 0
     part2 = 0
 
-    best = 0
-    current = defaultdict(lambda: 0)
+    sequence_prices = defaultdict(lambda: 0)
 
     for initial_secret_number in initial_secret_numbers:
-        secret_number = initial_secret_number
         seen = set()
-        q = deque()
+        q = list()
+
+        secret_number = initial_secret_number
         previous_ones = initial_secret_number % 10
+        
         for _ in range(2000):
             secret_number = evolve(secret_number)
+
             ones_digit = secret_number % 10
             q.append(ones_digit - previous_ones)
             previous_ones = ones_digit
+
             if len(q) < 4:
                 continue
-            key = tuple(q)
-            q.popleft()
-            if key in seen:
+
+            sequence = tuple(q)
+            q.pop(0)
+
+            if sequence in seen:
                 continue
-            seen.add(key)
-            current[key] += ones_digit
-            if current[key] > best:
-                best = current[key]
-                best_seq = key
+
+            seen.add(sequence)
+            sequence_prices[sequence] += ones_digit
         
         part1 += secret_number
 
     print("Part 1:", part1)
-    print("Part 2:", best)
+    print("Part 2:", max(sequence_prices.values()))
